2430bb7f5bdf9b83b76a15f5244904bb778061c2
Simon <simonbiss20@gmail.com>
2024-05-28 16:51:37 -0400

    page artiste deplacement du tableau

--- src/pages/artists.tsx
+++ src/pages/artists.tsx
@@ -162,16 +162,16 @@ const ArtistsPage: React.FC = () => {
   };
 
   return (
-    <div className="container mx-auto pt-20 px-4 py-6">
-      <div className='ml-8 w-full p-4'>
-      <DynamicTable
-        columns={columns}
-        data={artists.filter(artist => artist.name.toLowerCase().includes(searchTerm.toLowerCase()) || artist.email.toLowerCase().includes(searchTerm.toLowerCase()))}
-        searchTerm={searchTerm}
-        setSearchTerm={setSearchTerm}
-        addButtonText="Add an artist"
-        onAdd={() => setAddArtistPopupOpen(true)}
-      />
+    <div className="container mx-auto pt-20 px-4 py-6 ml-[120px]"> {/* Added margin-left to account for the sidebar */}
+      <div className='w-full p-4'>
+        <DynamicTable
+          columns={columns}
+          data={artists.filter(artist => artist.name.toLowerCase().includes(searchTerm.toLowerCase()) || artist.email.toLowerCase().includes(searchTerm.toLowerCase()))}
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+          addButtonText="Add an artist"
+          onAdd={() => setAddArtistPopupOpen(true)}
+        />
       </div>
       <AddArtistPopup isOpen={isAddArtistPopupOpen} onClose={() => setAddArtistPopupOpen(false)} onAddArtist={handleAddArtist} />
       {isDeleteModalOpen && (
